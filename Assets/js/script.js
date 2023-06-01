7c30aa837cf2fd4ee1d3b9edfd97c829b0fc6570
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-06-01 18:18:33 +0530

    update project section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -123,9 +123,7 @@ function showProjects(projects) {
         <div class="desc">
           <p class="project-description">${project.desc}</p>
           <br>
-          <h1 class="pp">✨Highlights :-</h1>
-           <p class="pp"> ➡️ ${project.highlights[0]}</p>
-            <p class="pp">➡️ ${project.highlights[1]}</p>
+       
           <div class="btns">
             <a href="${project.links.view}" class="btn project-deployed-link" target="_blank"><i class="fas fa-eye"></i> View</a>
             <a href="${project.links.code}" class="btn project-github-link" target="_blank">Code <i class="fas fa-code"></i></a>
