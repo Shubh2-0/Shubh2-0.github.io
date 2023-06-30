9c6e2c49ffd7a8a18f2b8dcf7614c6ee4dbb9477
Shubham Bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-06-30 11:10:22 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -37,22 +37,7 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
-    // <!-- emailjs to mail contact form data -->
-    // $("#contact-form").submit(function (event) {
-    //     emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
-    //     emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-    //         .then(function (response) {
-    //             console.log('SUCCESS!', response.status, response.text);
-    //             document.getElementById("contact-form").reset();
-    //             alert("Form Submitted Successfully");
-    //         }, function (error) {
-    //             console.log('FAILED...', error);
-    //             alert("Form Submission Failed! Try Again");
-    //         });
-    //     event.preventDefault();
-    // });
-    // <!-- emailjs to mail contact form data -->
 
 });
 
@@ -130,7 +115,7 @@ function showProjects(projects) {
           <div id="tech2" class="project-tech-stack"><img id="tech" src="https://skillicons.dev/icons?i=${project.stacks}" alt=""></div>
         </div>
       </div>
-    </div>`;
+    </div>`
   });
 }
 
